c4e1293c3e89fe79bbdd45eb4c79f8427d280796
Marc Aupont <mewsicmasta@gmail.com>
2021-08-07 14:43:11 -0400

    Initial Commit

---
+++ teststset/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  teststset
+//
+//  Created by Marc Aupont on 8/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ teststset/teststsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  teststsetApp.swift
+//  teststset
+//
+//  Created by Marc Aupont on 8/7/21.
+//
+
+import SwiftUI
+
+@main
+struct teststsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
